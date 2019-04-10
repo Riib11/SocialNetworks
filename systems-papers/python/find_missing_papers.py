84d0bd6d24d3bfeda3d6eddd1d9470cdacefd203
Henry <henry@reed.edu>
2019-04-10 08:59:15 -0700

    working on find_missing_papers.py

--- systems-papers/python/find_missing_papers.py
+++ systems-papers/python/find_missing_papers.py
@@ -7,22 +7,26 @@ known        = s2_data.get_dict_gA()
 known_titles = [ p["title"] for p in known.values() ]
 title_to_id  = { p["title"] : p_id for p_id, p in known.items() }
 
-confs = list(conf_data.get_each_conference_papers())
+conferences = list(conf_data.get_each_conference_papers())
+papers      = sum([ conf_papers for conf_name, conf_papers in conferences ], [])
+
+match_threshold = 5
+def is_match(s1, s2): return util_str.editDistance(s1, s2) <= match_threshold
 
 def is_known(paper_title):
   if paper_title in known_titles: return True
-  for known_title in tqdm(known_titles):
-    if util_str.editDistance(paper_title, known_title) <= 5:
-      return True
-  return False
+  return any ([ is_match(paper_title, known_title) for known_title in known_titles ])
 
 missing_titles = []
 
-for (conf_name, conf_papers) in confs:
-  for paper in conf_papers:
-    title = paper["title"]
-    if not is_known(title):
-      missing_titles.append(title)
-      print(title)
+for paper in tqdm(papers):
+  title = paper["title"]
+  if not is_known(title): missing_titles.append(title)
 
+print("\n".join(missing_titles))
+print("="*50)
 print("total:", len(missing_titles))
+
+result_fn = "find_missing_papers_result.txt"
+print("missing paper titles written to:", result_fn)
+with open(result_fn, "w+") as file: file.write("\n".join(missing_titles))
