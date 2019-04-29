c7b1feee89c14324c0bccd781e464847a3253174
Henry <henry@reed.edu>
2019-04-29 16:54:45 -0700

    created finalized paper_s2id => paper_key csv and json

--- systems-papers/python/conferences/combine_found_keys.py
+++ systems-papers/python/conferences/combine_found_keys.py
@@ -1,5 +1,27 @@
+import utils.json as u_json
+import utils.csv as u_csv
+
 PARENT_DIR = "conferences/"
 DATA_DIR   = PARENT_DIR + "data/"
 
-S2ID_KEY_FN = DATA_DIR + "s2_key.json"
-S2ID_KEY_NEW_FN = DATA_DIR + "s2_key"
+S2ID_KEY_OLD_FN = DATA_DIR + "s2id_key_old.json"
+KEY_S2ID_ADD_FN = DATA_DIR + "missing_key_to_found_s2id.json"
+S2ID_KEY_NEW_FN = DATA_DIR + "s2id_key.json"
+S2ID_KEY_NEW_CSV_FN = S2ID_KEY_NEW_FN.replace(".json", ".csv")
+
+# old
+s2id_key_old = u_json.load_json(S2ID_KEY_OLD_FN)
+
+# add
+key_s2id_add = u_json.load_json(KEY_S2ID_ADD_FN)
+s2id_key_add = { v:k for k,v in key_s2id_add.items() if v != None }
+
+# new
+s2id_key_new = { k:v for k,v in s2id_key_old.items() }
+
+for s2id, key in s2id_key_add.items():
+    s2id_key_new[s2id] = key
+
+# save new
+u_json.dump_json(s2id_key_new, S2ID_KEY_NEW_FN)
+u_csv.dump_csv(s2id_key_new, S2ID_KEY_NEW_CSV_FN)
--- systems-papers/python/utils/csv.py
+++ systems-papers/python/utils/csv.py
@@ -1,13 +1,26 @@
 import csv
+import utils.debug as debug
+from tqdm import tqdm
 
-def dict_to_csv(d, file):
-  writer = csv.write()
-  writer.writerow()
+def dump_csv(d, fn):
+    debug.log("writing csv to file: "+fn)
+    with open(fn, "w+") as file:
+        writer = csv.writer(file, delimiter=',')
+        for k,v in tqdm(d.items()):
+            writer.writerow([k,v])
 
-  writer = csv.writer(file)
-  node_ids = list(self.graph.nodes())
-  writer.writerow([""] + node_ids)
-  for node_i in range(len(node_ids)):
-    node_id = node_ids[node_i]
-    adjmat_row = list(map(str,adjmat_list[node_i]))
-    writer.writerow([node_id] + adjmat_row)
+# def dict_to_csv(d, file):
+#   writer = csv.write()
+#   writer.writerow()
+#
+#   writer = csv.writer(file)
+#   node_ids = list(self.graph.nodes())
+#   writer.writerow([""] + node_ids)
+#   for node_i in range(len(node_ids)):
+#     node_id = node_ids[node_i]
+#     adjmat_row = list(map(str,adjmat_list[node_i]))
+#     writer.writerow([node_id] + adjmat_row)
+
+if __name__ == "__main__":
+    d = {"hello": "world"}
+    dict_to_csv(d, "/Users/Henry/Downloads/test-dict.csv")
